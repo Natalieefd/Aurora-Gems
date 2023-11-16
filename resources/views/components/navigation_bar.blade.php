780cc0c1499ee057cc99e3412c8c70883df99b92
Natalieefd <nataliefuad2003@gmail.com>
2023-11-16 21:12:25 +0800

    feat: middleware

--- resources/views/components/navigation_bar.blade.php
+++ resources/views/components/navigation_bar.blade.php
@@ -2,15 +2,9 @@
     <div class="flex justify-between items-center pl-10 pr-10 text-peach">
         <ul class="flex gap-10">
             <li>
-                @if (Auth::check())
                 <a href="{{ route('pengguna.product') }}">
                     <p class="capitalize font-semibold">produk</p>
                 </a>
-                @else
-                <a href="{{ route('login') }}">
-                    <p class="capitalize font-semibold">produk</p>
-                </a>
-                @endif
             </li>
             <li>
                 <a href="{{ route('pengguna.dashboard') }}">
@@ -21,21 +15,12 @@
         <img src="{{ asset('assets/logo_aurora_gems_render.png') }}" alt="Aurora Gems" class="w-16">
         <ul class="flex gap-10">
             <li>
-                @if (Auth::check())
                 <a href="{{ route('pengguna.favorite') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path
                             d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                     </svg>
                 </a>
-                @else
-                <a href="{{ route('login') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                        <path
-                            d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                    </svg>
-                </a>
-                @endif
             </li>
             @if (Auth::check())
                 <li><a href="{{ route('logout') }}">
--- routes/web.php
+++ routes/web.php
@@ -26,93 +26,83 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-
 Route::post('/login/action', [
     AuthController::class,
     'loginAction'
 ])->name('login.action');
 
-
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
-
 
 Route::post('/register/action', [
     AuthController::class,
     'registerAction'
 ])->name('register.action');
 
-
-Route::get('/pengguna/dashboard', function() {
-    return view('pengguna.dashboard');
-})->name('pengguna.dashboard');
-
-
-Route::get('/pengguna/product', function () {
-    return view('pengguna.product', [
-        'product' => Product::all()
-    ]);
-})->name('pengguna.product');
-
-
-Route::get('/admin/dashboard', function() {
+Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/pengguna/dashboard', function () {
+        return view('pengguna.dashboard');
+    })->name('pengguna.dashboard');
 
-Route::get('/admin/manajemen_product', function () {
-    return view('admin.manajemen_product', [
-        'product' => Product::all()
-    ]);
-})->name('admin.manajemen_product');
+    Route::get('/pengguna/product', function () {
+        return view('pengguna.product', [
+            'product' => Product::all(),
+        ]);
+    })->name('pengguna.product');
 
+    // Route::get('/admin/dashboard', function () {
+    //     return view('admin.dashboard');
+    // })->name('admin.dashboard');
+
+    Route::get('/admin/manajemen_product', function () {
+        return view('admin.manajemen_product', [
+            'product' => Product::all(),
+        ]);
+    })->name('admin.manajemen_product');
+
+    Route::get('/admin/product', function () {
+        return view('admin.product', [
+            'product' => Product::all(),
+        ]);
+    })->name('admin.product');
+
+    Route::get('/admin/favorite', [
+        FavoriteController::class,
+        'relasi'
+    ])->name('admin.favorite');
+
+    Route::get('/pengguna/favorite', [
+        FavoriteController::class,
+        'relasipengguna'
+    ])->name('pengguna.favorite');
+
+    Route::get('/admin/favorite/{id}/action', [
+        FavoriteController::class,
+        'tambah'
+    ])->name('admin.favorite.action');
+
+    Route::get('/pengguna/favorite/{id}/action', [
+        FavoriteController::class,
+        'tambahpengguna'
+    ])->name('pengguna.favorite.action');
+});
 
 Route::controller(ProductController::class)->group(function () {
     Route::get('/admin/manajemen_product/tambah', 'tambah')->name('admin.add');
-    Route::post('/admin/manajemen_product/tambah/action','store')->name('admin.store');
+    Route::post('/admin/manajemen_product/tambah/action', 'store')->name('admin.store');
     Route::get('/admin/manajemen_product/edit/{id}', 'edit')->name('admin.edit');
-    Route::post('/admin/manajemen_product/edit/{id}/action','update')->name('admin.update');
+    Route::post('/admin/manajemen_product/edit/{id}/action', 'update')->name('admin.update');
     Route::post('/admin/manajemen_product/delete/{id}/action', 'delete')->name('admin.delete');
     Route::get('/admin/product_detail/show/{id}', 'showProductAdmin')->name('admin.product_detail');
     Route::get('/pengguna/product_detail/show/{id}', 'showProductPengguna')->name('pengguna.product_detail');
 });
 
-
-Route::get('/admin/product', function () {
-    return view('admin.product', [
-        'product' => Product::all()
-    ]);
-})->name('admin.product');
-
-Route::get('/admin/favorite',
-    [FavoriteController::class,
-    'relasi'
-    ]
-)->name('admin.favorite');
-
-Route::get('/pengguna/favorite',
-    [FavoriteController::class,
-    'relasipengguna'
-    ]
-)->name('pengguna.favorite');
-
-Route::get('/admin/favorite/{id}/action',
-    [FavoriteController::class,
-    'tambah'
-    ]
-)->name('admin.favorite.action');
-
-Route::get('/pengguna/favorite/{id}/action',
-    [FavoriteController::class,
-    'tambahpengguna'
-    ]
-)->name('pengguna.favorite.action');
-
-
 Route::get('/logout', [
     AuthController::class,
     'logout'
 ])->name('logout');
-
-
